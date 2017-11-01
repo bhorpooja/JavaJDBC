876428c2f53c14efa49f5873856ee14aa53235f5
pooja <bhorpooja222gmail.com>
2017-11-01 11:50:31 +0530

    alter table command

--- JavaJDBC/src/JdbcDemo.java
+++ JavaJDBC/src/JdbcDemo.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by pooja on 1/11/17.
@@ -14,8 +11,27 @@ public class JdbcDemo {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/Demo","root", "pooja");
             Statement st=con.createStatement();
-            String sql="create table Batch(id int(10),name varchar(20),city varchar(20))";
+
+            //create table command
+//            String sql="create table Batch(id int(10),name varchar(20),city varchar(20))";
+//            st.execute(sql);
+//            System.out.println("Table created successfully");
+
+
+            //select table command
+//            ResultSet rs=st.executeQuery("select * from Batch;");
+//            while (rs.next()){
+//                System.out.println(" "+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+//            }
+//            System.out.println("Data fetch Successfully");
+
+
+            //alter table command
+            String sql="alter table Batch add(dept varchar(20));";
             st.execute(sql);
+            System.out.println("Table alter successfully");
+            con.close();
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
