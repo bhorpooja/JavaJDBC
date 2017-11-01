cabb2e40e9674f0062936ebb3fa5c6a9c80f9fcd
pooja <bhorpooja222gmail.com>
2017-11-01 12:33:06 +0530

    update table command

--- JavaJDBC/src/JdbcDemo.java
+++ JavaJDBC/src/JdbcDemo.java
@@ -10,9 +10,10 @@ public class JdbcDemo {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/Demo","root", "pooja");
-            Statement st=con.createStatement();
+
 
             //create table command
+//            Statement st=con.createStatement();
 //            String sql="create table Batch(id int(10),name varchar(20),city varchar(20))";
 //            st.execute(sql);
 //            System.out.println("Table created successfully");
@@ -27,11 +28,28 @@ public class JdbcDemo {
 
 
             //alter table command
-            String sql="alter table Batch add(dept varchar(20));";
-            st.execute(sql);
-            System.out.println("Table alter successfully");
-            con.close();
+//            String sql="alter table Batch add(dept varchar(20));";
+//            st.execute(sql);
+//            System.out.println("Table alter successfully");
 
+
+            //insert command
+//            PreparedStatement ps=con.prepareStatement("insert into Batch values(?,?,?);");
+//            ps.setInt(1,4);
+//            ps.setString(2,"Sonal");
+//            ps.setString(3,"Nagpur");
+////            ps.setString(4,"Comp");
+////            ps.setFloat(5,67.23f);
+//            int count=ps.executeUpdate();
+//            System.out.println(count+" Row inserted successfully");
+
+            //update table
+//            Statement st=con.createStatement();
+//            String sql="update Batch set dept='E&TC', percentage=79.56 where name='pooja';";
+//            st.execute(sql);
+//            System.out.println("row updated successfully");
+
+            con.close();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
